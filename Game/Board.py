b8e34c4fc5302857841ae8ad69d50e8147587c0f
Ogheneovo Grant-Oyeye <ogheneovo.grant-oyeye@mail.mcgill.ca>
2022-01-21 10:46:06 -0500

    "basic grid drawn"

--- Game/Board.py
+++ Game/Board.py
@@ -1,12 +1,23 @@
+from random import random
 import pygame
+from scipy import rand
 from .Constants import WHITE, BLACK, ROWS, HEIGHT
+from .Cell import cell
 
+x = lambda r : 2 if r % 3 == 0 else 1
 class Board:
     def __init__(self):
-        self.cells = []
+        self.cells =  []
         self.solved = False
     def draw_self(self, window):
         window.fill(WHITE)
         for row in range(ROWS):
-            pygame.draw.line(window, BLACK, (row*(HEIGHT/ROWS), 0) , (row*(HEIGHT/ROWS),HEIGHT))
-            pygame.draw.line(window, BLACK, (0,row*(HEIGHT/ROWS)), (HEIGHT,row*(HEIGHT/ROWS)) )
+            pygame.draw.line(window, BLACK, (row*(HEIGHT/ROWS), 0) , (row*(HEIGHT/ROWS),HEIGHT),width = x(row))
+            pygame.draw.line(window, BLACK, (0,row*(HEIGHT/ROWS)), (HEIGHT,row*(HEIGHT/ROWS)),width = x(row))
+
+    def create_board():
+        for row in range(ROWS):
+            tmp = []
+            for col in range(ROWS):
+                tmp.append(cell(row,col,random()*9))
+             
---
+++ Game/Cell.py
@@ -0,0 +1,9 @@
+from re import X
+import pygame
+
+class cell:
+    def __init__(self,x,y,number):
+        self.x = x
+        self.y = y
+        self.number = number
+        
