24a558f3e8d1d943b9b4d6981b9d02eda011d578
Ogheneovo Grant-Oyeye <ogheneovo.grant-oyeye@mail.mcgill.ca>
2022-01-21 19:12:04 -0500

    Began Solving algorithm

--- Game/Board.py
+++ Game/Board.py
@@ -8,12 +8,17 @@ x = lambda r : 3 if r % 3 == 0 else 1
 class Board:
     def __init__(self):
         self.cellsList =  []
+        self.selected_cell = None
         self.solved = False
+    def select(self, x , y):
+        self.selected_cell = self.cellsList[x][y]
+        print(self.selected_cell.num)
 
     def draw_self(self, window):
         window.fill(WHITE)
         for row in range(ROWS):
             pygame.draw.line(window, BLACK, (row*(HEIGHT/ROWS), 0) , (row*(HEIGHT/ROWS),HEIGHT),width = x(row))
+        for row in range(ROWS + 1):
             pygame.draw.line(window, BLACK, (0,row*(HEIGHT/ROWS)), (HEIGHT,row*(HEIGHT/ROWS)),width = x(row))
         for row in range(len(self.cellsList)):
             for col in range(len(self.cellsList)):
@@ -23,8 +28,17 @@ class Board:
         for row in range(ROWS):
             tmp = []
             for col in range(ROWS):
-                num = round(random()*10)
+                num = round(random()*9)
                 tmp.append(cell(row,col,num))
                 print(num)
             self.cellsList.append(tmp)
+     #check if the current arrangement of numbers is a valid state       
+    def isValid(self,row,column,value):
+        valid_row = all([value != self.cellsList[x][row] for x in range(9)])
+        if valid_row:
+            valid_column = all([value != self.cellsList[column][x] for x in range(9)])
+            if valid_column:
+                print(x)
+
+
              
--- Sudoku.py
+++ Sudoku.py
@@ -1,25 +1,53 @@
+from math import floor
+from tkinter import S
 import pygame
 from Game.Constants import SQUARE_SIZE, WIDTH, HEIGHT;
 from Game.Board import Board
-WINDOW = pygame.display.set_mode((WIDTH,HEIGHT))
+WINDOW = pygame.display.set_mode((WIDTH,HEIGHT+60))
 pygame.display.set_caption('Sudoku')
 SQUARE_SIZE 
 pygame.font.init()
+import re
 
-FPS = 10
+FPS = 60
+digits = "[0-9]"
+getPos = lambda x : floor(x/(HEIGHT/9))*SQUARE_SIZE
+getRow = lambda x : floor(getPos(x)/SQUARE_SIZE)
 
+q = range(ord('0'),ord('9')+1)
+print(q)
 def main():
     run = True
     clock = pygame.time.Clock()
     board = Board()
     board.create_board()
     board.draw_self(WINDOW)
-
+    x = 0
+    y = 0
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if board.selected_cell == None:
+                    x,y = pygame.mouse.get_pos()
+                    board.select(getRow(x),getRow(y))
+                    pygame.draw.rect(WINDOW,(0,255,0),pygame.Rect(getPos(x),getPos(y),SQUARE_SIZE, SQUARE_SIZE),2)
+                else:
+                    board.draw_self(WINDOW)
+                    x,y = pygame.mouse.get_pos()
+                    board.select(getRow(x),getRow(y))
+                    pygame.draw.rect(WINDOW,(0,255,0),pygame.Rect(getPos(x),getPos(y),SQUARE_SIZE, SQUARE_SIZE),2)
+            elif event.type == pygame.KEYDOWN:
+                    if event.key in q:
+                        c = chr(event.key)
+                        print(c)
+                        board.selected_cell.num = c
+                        board.draw_self(WINDOW)
+                        pygame.draw.rect(WINDOW,(0,255,0),pygame.Rect(getPos(x),getPos(y),SQUARE_SIZE, SQUARE_SIZE),2)
+                
+                
         pygame.display.update()
     pygame.quit()
 
